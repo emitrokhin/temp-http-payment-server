305018e9b8a4b9ae569949426ffad42ef5baad3a
Pavel <pzajtsev@yandex.ru>
2024-10-16 11:44:51 +0400

    дописал новые тесты

--- src/test/java/ru/emitrohin/paymentserver/controller/ProfileControllerTest.java
+++ src/test/java/ru/emitrohin/paymentserver/controller/ProfileControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import ru.emitrohin.paymentserver.dto.TransactionResponse;
 import ru.emitrohin.paymentserver.dto.mapper.TransactionMapper;
 import ru.emitrohin.paymentserver.dto.profile.ProfileUpdateDTO;
+import ru.emitrohin.paymentserver.model.FirstRun;
 import ru.emitrohin.paymentserver.model.Profile;
 import ru.emitrohin.paymentserver.model.Transaction;
 import ru.emitrohin.paymentserver.service.FirstRunService;
@@ -28,6 +29,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.Collections.emptyList;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -110,15 +112,125 @@ public class ProfileControllerTest {
                 .andExpect(model().attribute("transactions", List.of(
                         new TransactionResponse(transaction1.getAmount(), transaction1.getDateTime(), transaction1.getCurrency()),
                         new TransactionResponse(transaction2.getAmount(), transaction2.getDateTime(), transaction2.getCurrency())
-                )));
+                )))
+                .andExpect(model().attribute("transactions", hasItem(
+                        allOf(
+                                hasToString(containsString("100")),
+                                hasToString(containsString(transaction1.getDateTime().toString())),
+                                hasToString(containsString("RUB"))
+                        )
+                )))
+
+                .andExpect(model().attribute("transactions", hasItem(
+                        allOf(
+                                hasToString(containsString("200")),
+                                hasToString(containsString(transaction2.getDateTime().toString())),
+                                hasToString(containsString("RUB"))
+                        )
+                )))
+        ;
+    }
+
+    @Test
+    @WithMockUser(username = "1234567890")
+    void getUser_ProfileExists_NoTransactions_ShouldReturnEmptyTransactions() throws Exception {
+        // Создаем тестовые объекты профиля
+        var profile = new Profile();
+        profile.setTelegramId(1234567890L);
+        profile.setFirstName("Pavel");
+        profile.setLastName("Zaytsev");
+        profile.setPhone("+1234567890");
+        profile.setEmail("pavel@zaytsev.com");
+        profile.setDateOfBirth(LocalDate.of(2004, 4, 20));
+        profile.setCity("Samara");
+        profile.setProfession("Developer");
+
+        // Создаем DTO для профиля
+        var profileDTO = new ProfileUpdateDTO(
+                "Pavel",
+                "Zaytsev",
+                "+1234567890",
+                "pavel@zaytsev.com",
+                LocalDate.of(2004, 4, 20),
+                "Samara",
+                "Developer");
+
+        // Настройка мока
+        when(profileService.findByTelegramId(1234567890L)).thenReturn(Optional.of(profile));
+        when(transactionService.getAllTransactions(anyLong())).thenReturn(emptyList()); // Имитация отсутствия транзакций
+        when(firstRunService.findFirstRun(1234567890L)).thenReturn(Optional.empty());
+        when(profileMapper.createUpdateResponse(profile)).thenReturn(profileDTO);
+
+        // Выполнение запроса
+        mockMvc.perform(get("/profile"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("profile"))
+                .andExpect(model().attributeExists("firstRun"))
+                .andExpect(model().attributeExists("profileForm"))
+                .andExpect(model().attributeExists("transactions"))
+                .andExpect(model().attribute("transactions", empty())); // Проверяем, что транзакций нет
+    }
+
+
+    @Test
+    @WithMockUser(username = "1234567890")
+    void getUser_FirstRun_ShouldReturnProfilePageWithLimitedFields() throws Exception {
+        // Создаем тестовые объекты профиля с полным набором данных
+        var profile = new Profile();
+        profile.setTelegramId(1234567890L);
+        profile.setFirstName("Pavel");
+        profile.setLastName("Zaytsev");
+        profile.setPhone("+1234567890");
+        profile.setEmail("pavel@zaytsev.com");
+        profile.setDateOfBirth(LocalDate.of(2004, 4, 20)); // Эти поля должны быть исключены при первом запуске
+        profile.setCity("Samara");
+        profile.setProfession("Developer");
+
+        // Создаем DTO с ограниченным набором полей (имя, фамилия, телефон, почта)
+        var limitedProfileDTO = new ProfileUpdateDTO(
+                "Pavel",
+                "Zaytsev",
+                "+1234567890",
+                "pavel@zaytsev.com",
+                null,  // Дата рождения не передается
+                null,  // Город не передается
+                null   // Профессия не передается
+        );
+
+        // Создаем запись первого запуска
+        var firstRun = new FirstRun();
+        firstRun.setTelegramId(1234567890L);
+
+        // Настройка мока
+        when(profileService.findByTelegramId(1234567890L)).thenReturn(Optional.of(profile));
+        when(firstRunService.findFirstRun(1234567890L)).thenReturn(Optional.of(firstRun)); // Симулируем первый запуск
+        when(profileMapper.createUpdateResponse(profile)).thenReturn(limitedProfileDTO); // Возвращаем DTO с ограниченными полями
+
+        // Выполнение запроса
+        mockMvc.perform(get("/profile"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("profile"))
+                .andExpect(model().attributeExists("firstRun")) // Проверяем наличие флага первого запуска
+                .andExpect(model().attributeExists("profileForm"))
+                .andExpect(model().attribute("profileForm", allOf(
+                        hasToString(containsString("Pavel")),
+                        hasToString(containsString("Zaytsev")),
+                        hasToString(containsString("+1234567890")),
+                        hasToString(containsString("pavel@zaytsev.com")),
+                        hasToString(containsString(nullValue().toString())), // Дата рождения не передается
+                        hasToString(containsString(nullValue().toString())), // Город не передается
+                        hasToString(containsString(nullValue().toString())) // Профессия не передается
+                )))
+                .andExpect(model().attributeExists("transactions"))
+                .andExpect(model().attribute("transactions", empty())); // Проверяем, что транзакций нет
     }
 
     @Test
     @WithMockUser(username = "1234567890")
     void getUser_ProfileNotFound_ShouldRedirectToHomePage() throws Exception {
         // Мокируем SecurityContext
-        Authentication authentication = Mockito.mock(Authentication.class);
-        SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+        var authentication = Mockito.mock(Authentication.class);
+        var securityContext = Mockito.mock(SecurityContext.class);
         when(securityContext.getAuthentication()).thenReturn(authentication);
         when(authentication.getName()).thenReturn("1234567890"); // Пример Telegram ID
         SecurityContextHolder.setContext(securityContext);
