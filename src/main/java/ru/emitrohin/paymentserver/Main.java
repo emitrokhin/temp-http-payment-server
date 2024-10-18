a7ea43f7d7595f4ed6506914eaca0bd0d62d80fd
Evgenii Mitrokhin <10193179+emitrokhin@users.noreply.github.com>
2024-10-18 18:24:47 +0300

    Интеграция bugsnag сервер и клиент

--- src/main/java/ru/emitrohin/paymentserver/Main.java
+++ src/main/java/ru/emitrohin/paymentserver/Main.java
@@ -9,13 +9,12 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 //TODO remove jsessionid
 //TODO implement roles - paid, admin
 //TODO remove all unnecessary comments in html
-//TODO send log to ??
 //TODO reinvent auth
 //TODO fix migration transactionId was unique
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
 
-        //TODO on init check bot is connected and cloudpayments is ok
+        //TODO on init check bot is connected and cloudpayments is ok and bugsnag is ok
     }
 }
