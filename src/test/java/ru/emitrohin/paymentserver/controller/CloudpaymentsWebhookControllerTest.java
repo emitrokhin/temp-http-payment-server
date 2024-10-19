b8bb0dc0b86b00dc68d0673b34af97d81b4efcc4
Pavel <pzajtsev@yandex.ru>
2024-10-20 00:20:23 +0400

    дописал тесты

--- src/test/java/ru/emitrohin/paymentserver/controller/CloudpaymentsWebhookControllerTest.java
+++ src/test/java/ru/emitrohin/paymentserver/controller/CloudpaymentsWebhookControllerTest.java
@@ -1,58 +1,39 @@
 package ru.emitrohin.paymentserver.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import ru.emitrohin.paymentserver.client.BotMotherClient;
 import ru.emitrohin.paymentserver.client.TelegramBotClient;
-import ru.emitrohin.paymentserver.config.TestSecurityConfig;
 import ru.emitrohin.paymentserver.dto.cloudpayments.CloudpaymentsRequest;
+import ru.emitrohin.paymentserver.dto.mapper.CardMapper;
 import ru.emitrohin.paymentserver.dto.mapper.CloudpaymentRequestMapper;
 import ru.emitrohin.paymentserver.dto.mapper.ProfileMapper;
 import ru.emitrohin.paymentserver.dto.mapper.TransactionMapper;
 import ru.emitrohin.paymentserver.model.Card;
+import ru.emitrohin.paymentserver.model.SubscriptionStatus;
 import ru.emitrohin.paymentserver.model.Transaction;
 import ru.emitrohin.paymentserver.repository.CardRepository;
 import ru.emitrohin.paymentserver.repository.SubscriptionRepository;
 import ru.emitrohin.paymentserver.repository.TransactionRepository;
 import ru.emitrohin.paymentserver.security.TelegramPreAuthenticatedProcessingFilter;
 import ru.emitrohin.paymentserver.service.*;
-import ru.emitrohin.paymentserver.util.IpUtil;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
 import java.math.BigDecimal;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
-import java.util.Base64;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -117,6 +98,9 @@ public class CloudpaymentsWebhookControllerTest {
     @MockBean
     private SubscriptionRepository subscriptionRepository;
 
+    @MockBean
+    private CardMapper cardMapper;
+
 
     private static final Long TELEGRAM_ID = 1234567890L;
     private static final Transaction TEST_TRANSACTION1 = createTestTransaction(BigDecimal.valueOf(100),
@@ -180,7 +164,6 @@ public class CloudpaymentsWebhookControllerTest {
     @Test
     @WithMockUser(username = "1234567890")
     void successWebhook_ShouldSaveTransactionAndCardAndSendTelegramMessage() throws Exception {
-
         // Мокируем SecurityContext
         var authentication = Mockito.mock(Authentication.class);
         var securityContext = Mockito.mock(SecurityContext.class);
@@ -191,12 +174,14 @@ public class CloudpaymentsWebhookControllerTest {
 
         var transactionRequest = createTestRequest();
 
-        // Настройка мока для сервисов
-//        doNothing().when(transactionService).save(TEST_TRANSACTION1);
-        when(cardService.getCardByCardId(transactionRequest.getCardId())).thenReturn(null);
-//        doNothing().when(cardService).saveCard(TEST_CARD1);
-//        doNothing().when(subscriptionService).createOrUpdateCurrentSubscriptionStatus(eq(TELEGRAM_ID), any());
 
+        // Настройка мока для сервисов
+        when(cardService.getCardByCardId(transactionRequest.getCardId())).thenReturn(null);
+        cardService.saveCard(TEST_CARD1);
+        transactionService.save(TEST_TRANSACTION1);
+        subscriptionService.createOrUpdateCurrentSubscriptionStatus(TELEGRAM_ID, SubscriptionStatus.PAID);
+        telegramBotClient.sendMessageWithButtons("Твоя подписка успешно оплачена! 🎉\n\nВот ссылки для твоего удобства \uD83D\uDC47", TELEGRAM_ID);
+        botMotherClient.sendPayload(TELEGRAM_ID);
         // Запрос
         var requestBuilder = post("/cloudpayments/success")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -208,12 +193,119 @@ public class CloudpaymentsWebhookControllerTest {
                 .andExpect(jsonPath("$.code").value(0));
 
         // Проверка, что транзакция и карта были сохранены
-        verify(transactionService).save(any());
+        verify(transactionService).save(TEST_TRANSACTION1);
         verify(cardService).saveCard(TEST_CARD1);
-//
-//        // Проверка, что были вызваны другие методы
+        // Проверка, что были вызваны другие методы
         verify(subscriptionService).createOrUpdateCurrentSubscriptionStatus(eq(TELEGRAM_ID), any());
         verify(telegramBotClient).sendMessageWithButtons(anyString(), eq(TELEGRAM_ID));
         verify(botMotherClient).sendPayload(eq(TELEGRAM_ID));
     }
+
+    @Test
+    @WithMockUser(username = "1234567890")
+    void successWebhook_ShouldSaveTransactionAndSendTelegramMessage_WhenCardExists() throws Exception {
+        // Мокируем SecurityContext
+        var authentication = Mockito.mock(Authentication.class);
+        var securityContext = Mockito.mock(SecurityContext.class);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getName()).thenReturn("1234567890");
+
+        SecurityContextHolder.setContext(securityContext);
+
+        var transactionRequest = createTestRequest();
+
+        // Настройка мока для сервисов
+        when(cardService.getCardByCardId(transactionRequest.getCardId())).thenReturn(TEST_CARD1); // Карта найдена
+        transactionService.save(TEST_TRANSACTION1);
+        subscriptionService.createOrUpdateCurrentSubscriptionStatus(TELEGRAM_ID, SubscriptionStatus.PAID);
+        telegramBotClient.sendMessageWithButtons("Твоя подписка успешно оплачена! 🎉\n\nВот ссылки для твоего удобства \uD83D\uDC47", TELEGRAM_ID);
+        botMotherClient.sendPayload(TELEGRAM_ID);
+
+        // Запрос
+        var requestBuilder = post("/cloudpayments/success")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(transactionRequest));
+
+        // Выполнение запроса
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(0));
+
+        // Проверка, что транзакция была сохранена
+        verify(transactionService).save(TEST_TRANSACTION1);
+        // Проверка, что карта не сохранялась
+        verify(cardService, never()).saveCard(TEST_CARD1);
+        // Проверка, что другие методы были вызваны
+        verify(subscriptionService).createOrUpdateCurrentSubscriptionStatus(eq(TELEGRAM_ID), any());
+        verify(telegramBotClient).sendMessageWithButtons(anyString(), eq(TELEGRAM_ID));
+        verify(botMotherClient).sendPayload(eq(TELEGRAM_ID));
+    }
+
+    @Test
+    @WithMockUser(username = "1234567890")
+    void failWebhook_ShouldSaveTransactionAndDeactivateCard_WhenCardExists() throws Exception {
+        // Мокируем SecurityContext
+        var authentication = Mockito.mock(Authentication.class);
+        var securityContext = Mockito.mock(SecurityContext.class);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getName()).thenReturn("1234567890");
+
+        SecurityContextHolder.setContext(securityContext);
+
+        var transactionRequest = createTestRequest();
+
+        // Настройка мока для сервисов
+        when(cardService.getCardByCardId(transactionRequest.getCardId())).thenReturn(TEST_CARD1);
+        cardService.deactivateCard(transactionRequest.getCardId());
+        transactionService.save(TEST_TRANSACTION1);
+
+        // Запрос
+        var requestBuilder = post("/cloudpayments/fail")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(transactionRequest));
+
+        // Выполнение запроса
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(0));
+
+        // Проверка, что транзакция была сохранена
+        verify(transactionService).save(TEST_TRANSACTION1);
+        // Проверка, что карта была деактивирована
+        verify(cardService).deactivateCard(TEST_CARD1.getCardId());
+    }
+
+    @Test
+    @WithMockUser(username = "1234567890")
+    void failWebhook_ShouldSaveTransactionAndNotDeactivateCard_WhenCardDoesNotExist() throws Exception {
+        // Мокируем SecurityContext
+        var authentication = Mockito.mock(Authentication.class);
+        var securityContext = Mockito.mock(SecurityContext.class);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getName()).thenReturn("1234567890");
+
+        SecurityContextHolder.setContext(securityContext);
+
+        var transactionRequest = createTestRequest();
+
+        // Настройка мока для сервисов
+        when(cardService.getCardByCardId(transactionRequest.getCardId())).thenReturn(null); // Карта не найдена
+        transactionService.save(TEST_TRANSACTION1);
+
+        // Запрос
+        var requestBuilder = post("/cloudpayments/fail")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(transactionRequest));
+
+        // Выполнение запроса
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(0));
+
+        // Проверка, что транзакция была сохранена
+        verify(transactionService).save(TEST_TRANSACTION1);
+        // Проверка, что деактивация карты не выполнялась
+        verify(cardService, never()).deactivateCard(anyString());
+    }
+
 }
