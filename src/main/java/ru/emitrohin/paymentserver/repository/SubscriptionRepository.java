48787633d8b5cbd51af164aa01f6dc2c019bec9e
Pavel <pzajtsev@yandex.ru>
2024-10-25 15:05:26 +0400

    добавил методы для запросов к бд

--- src/main/java/ru/emitrohin/paymentserver/repository/SubscriptionRepository.java
+++ src/main/java/ru/emitrohin/paymentserver/repository/SubscriptionRepository.java
@@ -6,6 +6,7 @@ import ru.emitrohin.paymentserver.model.SubscriptionStatus;
 
 import java.time.LocalDateTime;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
@@ -16,4 +17,10 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Long
             SubscriptionStatus subscriptionStatus,
             LocalDateTime startOfMonth,
             LocalDateTime endOfMonth);
+    Optional<Subscription> findByTelegramIdAndSubscriptionStatus(Long telegramId, SubscriptionStatus subscriptionStatus);
+    Optional<Subscription> findFirstByTelegramIdAndSubscriptionEndDateAfter(Long telegramId, LocalDateTime endDate);
+    List<Subscription> findAllBySubscriptionEndDateBeforeAndSubscriptionStatus(LocalDateTime endDate, SubscriptionStatus subscriptionStatus);
+    Optional<Subscription> findFirstByTelegramIdAndSubscriptionEndDateBefore(Long telegramId, LocalDateTime endDate);
+    Optional<Subscription> findFirstByTelegramId(Long telegramId);
+    List<Subscription> findAllBySubscriptionEndDateAfterAndSubscriptionStatus(LocalDateTime endDate, SubscriptionStatus subscriptionStatus);
 }
